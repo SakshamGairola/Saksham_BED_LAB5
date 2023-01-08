4438cb5ab73fd0b081172974dbdfbd7f39ec94d8
Saksham <saksham.gairola28@gmail.com>
2023-01-08 13:51:57 +0530

    Initial Commit

---
+++ src/main/java/com/greatLearning/SakshamBedLab5Application.java
@@ -0,0 +1,13 @@
+package com.greatLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakshamBedLab5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakshamBedLab5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/greatLearning/SakshamBedLab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakshamBedLab5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
