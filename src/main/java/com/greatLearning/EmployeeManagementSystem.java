d8aeed8bc2b0d0f93209c1e717cac8bcc7d0ecdb
Saksham <saksham.gairola28@gmail.com>
2023-01-08 14:00:22 +0530

    Initial Commit

--- src/main/java/com/greatLearning/EmployeeManagementSystem.java
+++ src/main/java/com/greatLearning/EmployeeManagementSystem.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SakshamBedLab5Application {
+public class EmployeeManagementSystem {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SakshamBedLab5Application.class, args);
+		SpringApplication.run(EmployeeManagementSystem.class, args);
 	}
 
 }
